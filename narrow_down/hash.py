4b5a7d116cd44c7a9800bf3ebf1699e3d97708fb
chr1st1ank <chr1st1ank@krudewig-online.de>
2021-12-31 14:54:37 +0100

    Adding more hash functions

--- narrow_down/hash.py
+++ narrow_down/hash.py
@@ -1,4 +1,4 @@
 """Hash algorithms."""
-from ._rust import murmur3_32bit
+from ._rust import murmur3_32bit, xxhash_32bit, xxhash_64bit
 
-__all__ = ["murmur3_32bit"]
+__all__ = ["murmur3_32bit", "xxhash_32bit", "xxhash_64bit"]
--- tests/test_hash.py
+++ tests/test_hash.py
@@ -2,7 +2,16 @@
 from narrow_down import hash
 
 
-def test_murmur_rust():
-    import murmurhash.mrmr
+def test_murmur3_32bit():
+    assert hash.murmur3_32bit("test") == 3127628307
+    assert hash.murmur3_32bit("") == 0
 
-    assert murmurhash.mrmr.hash("abcd".encode("utf-8")) == hash.murmur3_32bit("abcd")
+
+def test_xxhash_32bit():
+    assert hash.xxhash_32bit("test") == 1042293711
+    assert hash.xxhash_32bit("") == 46947589
+
+
+def test_xxhash_64bit():
+    assert hash.xxhash_64bit("test") == 5754696928334414137
+    assert hash.xxhash_64bit("") == 17241709254077376921
