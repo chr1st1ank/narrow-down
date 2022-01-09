ad3be642954f7ddea1d02fb4e84b3fa9c1f44f8e
chr1st1ank <chr1st1ank@krudewig-online.de>
2022-01-09 13:23:07 +0100

    Try a prerelease to test deployment

--- narrow_down/__init__.py
+++ narrow_down/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Christian Krudewig"""
 __email__ = "chr1st1ank@krudewig-online.de"
-__version__ = "0.1.1"
+__version__ = "0.2.0-alpha1"
 
 from . import data_types, hash, similarity_store, storage  # noqa
