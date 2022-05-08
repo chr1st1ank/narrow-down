97e8f88689cf055a30234977d342d778222a50ec
chr1st1ank <chr1st1ank@krudewig-online.de>
2022-05-08 19:58:00 +0200

    Bump version: 0.9.3 → 0.10.0

--- narrow_down/__init__.py
+++ narrow_down/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Christian Krudewig"""
 __email__ = "chr1st1ank@krudewig-online.de"
-__version__ = "0.9.3"
+__version__ = "0.10.0"
 
 from . import hash, similarity_store, storage  # noqa
