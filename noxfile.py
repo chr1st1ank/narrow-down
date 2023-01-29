cc58d13f4209a362ef6c61ee4d55bb01fd40ac77
chr1st1ank <chr1st1ank@krudewig-online.de>
2023-01-29 12:16:41 +0100

    Add pypy3.9 to the nox settings

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ import nox
 from nox.sessions import Session
 
 nox.options.sessions = ["tests", "benchmarks"]
-python_versions = ["3.7", "3.8", "3.9", "3.10"]
+python_versions = ["3.7", "3.8", "3.9", "3.10", "pypy3.9"]
 
 
 def install_with_constraints(session: Session, *args: str) -> None:  # noqa
